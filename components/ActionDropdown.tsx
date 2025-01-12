3fdb94411ac80220210efe5db4d38eb846db4132
WeberDotExe <ctaufiquee786@gmail.com>
2025-01-12 18:20:49 +0530

    first commit

---
+++ components/ActionDropdown.tsx
@@ -0,0 +1,138 @@
+"use client";
+import React, { useState } from 'react'
+
+import {Dialog, DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    } from "@/components/ui/dialog"
+  
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import Image from 'next/image';
+import { Models } from 'node-appwrite';
+import { actionsDropdownItems } from '@/constants';
+import Link from 'next/link';
+import { constructDownloadUrl } from '@/lib/utils';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
+// import { rename } from '@/lib/actions/file.action';
+import { log } from 'console';
+import { deleteFile, renameFile, updateFileUsers } from '@/lib/actions/file.action';
+import { FileDetails,ShareInput } from './ActionsModalContent';
+// import ShareInput from './ShareInput';
+// import { rename } from 'fs';
+  
+
+const ActionDropdown = ({file}:{file:Models.Document}) => {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+    const [action, setAction] = useState<ActionType | null>(null)
+    const [name, setName] = useState(file.name)
+    const [isLoading, setIsLoading] = useState(false)
+    const [emails, setEmails] = useState<string[]>([])
+
+    const path = usePathname()
+
+    const closeAllModals = () => {
+        setIsModalOpen(false);
+        setIsDropdownOpen(false);
+        setAction(null);
+        setName(file.name);
+
+    }
+
+    const handleAction = async ()=> {
+      if(!action) return;
+      setIsLoading(true)
+      let success = false
+
+      const actions = {
+        rename: ()=> renameFile({fileId:file.$id,name,extension:file.extension,path}), 
+        share: () => updateFileUsers({fileId:file.$id,emails,path}),
+        delete: () => deleteFile({fileId:file.$id,path,bucketFileId:file.bucketFileId}),                
+      }
+      success = await actions[action.value as keyof typeof actions]();
+      if (success) closeAllModals();
+      setIsLoading(false)
+    }
+
+    const handleRemoveUser = async(email:string) =>{
+        const updatedEmails = emails.filter((e)=> e!== email);
+
+        const success = await updateFileUsers({fileId: file.$id,emails: updatedEmails, path})
+        if (success) setEmails(updatedEmails)
+          closeAllModals()
+    }
+
+    const renderDialogueContent = ( ) => {
+        if(!action) return null;
+        const {value,label} = action;
+        return ( 
+        <DialogContent className='shad-dialog-button'>
+        <DialogHeader className='flex flex-col gap-3'>
+          <DialogTitle className='text-center text-light-100'>
+            {label}
+          </DialogTitle>
+         {value === "rename" && <Input type='text' value={name} onChange={(e)=> setName(e.target.value)}/>}
+         {value === "details" && <FileDetails file={file} />}
+         {value === "share" && (<ShareInput file={file} onInputChange={setEmails} onRemove={handleRemoveUser} />)}
+         {value === "delete" && (<p className='delete-confirmation'>
+          Are you sure you want to delete {` `} <span className='delete-file-name'>{file.name}</span>?
+         </p>)}
+        </DialogHeader>
+        {["rename","delete","share"].includes(value)&& (
+            <DialogFooter className='flex flex-col gap-3 md:flex-row'>
+                <Button onClick={closeAllModals} className='modal-cancel-button'>
+                    Cancel
+                </Button>
+                <Button onClick={handleAction} className='modal-submit-button'>
+                    <p className='capitialize'>{value}</p>
+                    {isLoading && (<Image src='/assets/icons/loader.svg' width={24} height={24} alt='loader' className='animate-spin' />)}
+                </Button>
+            </DialogFooter>
+        )}
+      </DialogContent>)
+    };
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+  <DropdownMenuTrigger className='shad-no-focus'>
+    <Image src='/assets/icons/dots.svg' width={34} height={34} alt='dots' />
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel className='max-w-[200px] truncate'>
+        {file.name}
+    </DropdownMenuLabel>
+    <DropdownMenuSeparator />
+   {actionsDropdownItems.map((actionItem)=>(
+    <DropdownMenuItem key={actionItem.value} className='shad-dropdown-item' onClick={()=>{
+        setAction(actionItem)
+        if(["rename","share","delete","details"].includes(actionItem.value)){setIsModalOpen(true)}
+    }}>
+        {actionItem.value === "download" ? <Link href={constructDownloadUrl(file.bucketFileId)} download={file.name} className='flex item-center gap-2' >
+            <Image src={actionItem.icon} width={30} height={30} alt={actionItem.label}  />
+            {actionItem.label}
+        </Link>: <div className='flex item-center gap-2' >
+        <Image src={actionItem.icon} width={30} height={30} alt={actionItem.label}  />
+        {actionItem.label}
+        </div> }
+    </DropdownMenuItem>
+   ))}
+  </DropdownMenuContent>
+</DropdownMenu>
+   {renderDialogueContent()}
+</Dialog>
+
+  )
+}
+
+export default ActionDropdown
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,104 @@
+"use server"
+
+import { ID, Query } from "node-appwrite";
+ import { createAdminClient, createSessionClient } from "../appwrite";
+ import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
+import { parse } from "path";
+import { redirect } from "next/navigation";
+import { error } from "console";
+
+ const getUserByEmail = async (email : string) => {
+    const {databases} = await createAdminClient();
+    const result = await databases.listDocuments(appwriteConfig.databaseId, appwriteConfig.usersCollectionId,[Query.equal("email",[email])]);
+
+    return result.total > 0 ? result.documents[0] : null; 
+ };
+
+    const handleError = (error:unknown,message:string) => {
+        console.error(error,message);
+        throw error;
+    }
+
+ export const sendEmailOtp = async ({email}:{email:string}) => {
+    const {account} = await createAdminClient();
+
+    try {
+        const session = await account.createEmailToken(ID.unique(),email);
+        return session.userId;
+    } catch (error) {
+        handleError(error,"Failed to send email OTP");
+    }
+  };
+
+export const createAccount = async ({fullname,email}:{fullname:string;email:string}) => {
+    const existingUser = await getUserByEmail(email); 
+
+    const accountId = await sendEmailOtp({email}); 
+    if(!accountId) throw new Error("Failed to send an OTP");
+
+    if(!existingUser ){
+        const {databases} = await createAdminClient();
+        await databases.createDocument(appwriteConfig.databaseId,appwriteConfig.usersCollectionId,ID.unique(),{fullname,email,avatar:"/assets/icons/avatar.jpeg",accountId},);
+       
+    }
+        return parseStringify({accountId});      
+};
+
+export const verifySecret = async ({accountId,password}:{accountId:string;password:string})=>{
+    try {
+        const {account} = await createAdminClient();
+
+        const session = await account.createSession(accountId,password);
+
+        (await cookies()).set("appwrite-session",session.secret,{
+            path:"/",
+            httpOnly:true,
+            sameSite:"strict",
+            secure:true,           
+        });
+        return parseStringify({accountId:session.$id});
+    } catch (error) {
+        handleError(error,"Failed to verify OTP");
+    }
+};
+
+export const getCurrentUser = async () => {
+    const {account,databases} = await createSessionClient();
+
+    const result = await account.get();
+
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,appwriteConfig.usersCollectionId,[Query.equal("accountId",result.$id)]
+    );
+    if (user.total<=0) return null;
+    return parseStringify(user.documents[0]);
+    
+};
+
+export const signOutUser = async () => {
+    const {account} = await createSessionClient();
+
+    try {
+        await account.deleteSession("current");
+        (await cookies()).delete("appwrite-session");
+    } catch (error) {
+        handleError(error,"Failed to sign out");
+    } finally {
+        redirect("/sign-in");
+    }
+};
+
+export const signInUser = async ({email}:{email:string}) => {
+    try {
+        const existingUser = await getUserByEmail(email);
+        if(existingUser) {
+            return parseStringify({accountId:existingUser.accountId});
+        }
+        return parseStringify({accountId:null,error:"User not found"});
+
+    } catch (error) {
+        handleError(error,"Failed to sign in user");
+    }
+}
